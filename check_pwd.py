09ab3703f96fe5269d504104cad71b2a081269ec
aclos3 <45724446+aclos3@users.noreply.github.com>
2020-07-20 17:34:23 -0700

    Implementation Test 6
    I had to do some refactoring and restructuring of the code here.  Globals were added to contain all valid numbers and lower case letters. These strings are tested against each character in the test string and a count is made.  There must be at least 1 lower case to pass.

--- check_pwd.py
+++ check_pwd.py
@@ -4,16 +4,26 @@
 #    Date: 7/27/2020
 #    Description: contains the password checking function
 ###############################################################################
-
+VALID_NUMS = '1234567890'
+VALID_LOWER = 'abcdefghijklmnopqrstuvwxyz'
 
 def check_pwd(testStr):
+    numCount = 0
+    lowerCount = 0
+
     if(len(testStr) < 8):
         return False
     
     for x in range (0, len(testStr)):    
-        if(testStr[x] == 'a'):
-            return False
-        if(int(testStr[x]) >= 0 and int(testStr[x]) <= 9):
+        for y in range (0, len(VALID_LOWER)):
+            if(testStr[x] == VALID_LOWER[y]):
+                lowerCount = lowerCount + 1
+                print("lower count!")
+        for y in range (0, len(VALID_NUMS)):
+            if(testStr[x] == VALID_NUMS[y]):
+                numCount = numCount + 1
+        
+        if(lowerCount > 0):
             return False
 
     return True
