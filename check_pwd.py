c7d8a7c41795e22d2b6231c3ea66df36bebe5c7d
aclos3 <45724446+aclos3@users.noreply.github.com>
2020-07-20 17:48:20 -0700

    Implementation Test 8
    I fixed a logical error where I was returning false if upperCount or numCount were greater than 0.  Instead, we want to return false if any of them are equal to zero.  I also added the special character string and a for loop to test for and count those.

--- check_pwd.py
+++ check_pwd.py
@@ -7,26 +7,34 @@
 VALID_NUMS = '1234567890'
 VALID_LOWER = 'abcdefghijklmnopqrstuvwxyz'
 VALID_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+VALID_SPECIAL = '~`!@#$%^&*()_+-='
 
 def check_pwd(testStr):
     numCount = 0
     lowerCount = 0
     upperCount = 0
+    specialCount = 0
 
     if(len(testStr) < 8):
         return False
     
     for x in range (0, len(testStr)):    
+        
         for y in range (0, len(VALID_LOWER)):
             if(testStr[x] == VALID_LOWER[y]):
                 lowerCount = lowerCount + 1
             if(testStr[x] == VALID_UPPER[y]):
                 upperCount = upperCount + 1    
+        
         for y in range (0, len(VALID_NUMS)):
             if(testStr[x] == VALID_NUMS[y]):
                 numCount = numCount + 1
         
-        if(lowerCount > 0 or upperCount > 0):
+        for y in range (0, len(VALID_SPECIAL)):
+            if(testStr[x] == VALID_SPECIAL[y]):
+                specialCount = specialCount + 1
+        
+        if(lowerCount == 0 or upperCount == 0 or specialCount == 0):
             return False
 
     return True
